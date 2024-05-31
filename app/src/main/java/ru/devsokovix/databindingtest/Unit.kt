f6b34ed5b1fdcfb8f2d27881630a3ff5c3d9e80b
morits <mor1ts@mail.ru>
2024-05-31 17:55:55 +0300

    Добавлено суммирования данных и  отображения страки с условием

--- app/src/main/java/ru/devsokovix/databindingtest/Unit.kt
+++ app/src/main/java/ru/devsokovix/databindingtest/Unit.kt
@@ -1,3 +1,11 @@
 package ru.devsokovix.databindingtest
 
-data class Unit(var str: Int, var dex: Int, var wis: Int, var charPoint: Int)
+import androidx.databinding.BaseObservable
+
+data class Unit(var str: Int, var dex: Int, var wis: Int, var charPoint: Int) : BaseObservable() {
+    fun reset(){
+        charPoint += str + dex + wis;
+        str = 0; dex = 0; wis = 0;
+        notifyChange()
+    }
+}
